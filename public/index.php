6e4d78d36932d0952508aeb78183626d7ef1a2ae
atnartur <i@atnartur.ru>
2017-05-23 02:23:05 +0300

    + db container

--- public/index.php
+++ public/index.php
@@ -1,14 +1,37 @@
 <?php
 
+use Phalcon\Di\FactoryDefault;
 use Phalcon\Mvc\Micro;
 
-$app = new Micro();
+$di = new FactoryDefault();
 
-$app->get(
-    "/",
-    function () {
-        echo "<h1>Welcome from Phalcon!</h1>";
+$di->setShared('db', function () use ($di) {
+    return new \Phalcon\Db\Adapter\Pdo\Postgresql(array(
+        "host" => 'db',
+        "username" => 'test',
+        "password" => 'password',
+        "dbname" => 'test'
+    ));
+});
+
+$app = new Micro($di);
+
+$app->get("/", function () use ($app) {
+    echo "<h1>Welcome from Phalcon!</h1>";
+    echo '<h2>Collumns from table users</h2>';
+    echo '<pre>';
+
+    $res = $app['db']->query('SELECT *
+        FROM information_schema.columns
+        WHERE table_schema = \'public\'
+          AND table_name   = \'users\'
+        ')->fetchAll();
+
+    foreach ($res as $row) {
+        var_dump($row['column_name']);
     }
-);
+
+    echo '</pre>';
+});
 
 $app->handle();
